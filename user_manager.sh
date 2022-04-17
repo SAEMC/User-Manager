009408d8a4f3d32fadd1c3ee92fcdcf579c49249
SAEMC <saemc27@gmail.com>
2022-04-17 23:00:15 +0900

    Create user_manager.sh

---
+++ user_manager.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "[[ Linux User Manager ]]"
+read -p "What do you want? [add/del]: " cmd
+read -p "Enter the username: " user_name
+
+case $cmd in
+    "add"|"Add"|"ADD" )
+        sudo adduser $user_name
+
+        read -p "Wanna add ${user_name} to SUDO? [y/n]: " yn
+        if [[ "$yn" == "y" ]]; then
+            sudo usermod -aG sudo $user_name
+        fi
+        ;;
+    "del"|"Del"|"DEL" )
+        sudo deluser $user_name
+
+        read -p "Wanna remove ${user_name} directory? [y/n]: " yn
+        if [[ "$yn" == "y" ]]; then
+            sudo rm -r /home/$user_name
+        fi
+        ;;
+    * )
+        echo "Bye bye!"
+        ;;
+esac
+
+exit 0
