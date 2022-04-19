42f524fc4f40391391698ce506ba0cdf538b8e79
SAEMC <saemc27@gmail.com>
2022-04-19 23:32:44 +0900

    Update user_manager.sh

--- user_manager.sh
+++ user_manager.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo "[[ Linux User Manager ]]"
-read -p "What do you want? [add/del]: " cmd
+read -p "What do you want? [Add/Del]: " cmd
 read -p "Enter the username: " user_name
 
 case $cmd in
-    "add"|"Add"|"ADD" )
-        sudo adduser $user_name
-
-        read -p "Wanna add ${user_name} to SUDO? [y/n]: " yn
-        if [[ "$yn" == "y" ]]; then
-            sudo usermod -aG sudo $user_name
-			sudo sed -i "/${user_name} ALL=NOPASSWD: ALL/d" /etc/sudoers
-        fi
+    [Aa][Dd][Dd] )
+		sudo adduser $user_name
+		
+		read -p "Wanna add ${user_name} to SUDO? [Y/N]: " yn
+		if [[ "$yn" == "Y" || "$yn" == "y" ]]; then
+			sudo usermod -aG sudo $user_name
+			sudo echo "${user_name} ALL=NOPASSWD: ALL" >>/etc/sudoers
+		fi
         ;;
-    "del"|"Del"|"DEL" )
+    [Dd][Ee][Ll] )
         sudo deluser $user_name
-		sudo sed -i "/${user_name} ALL=NOPASSWD: ALL/d" /etc/sudoers
+		sudo sed -i '' "/${user_name} ALL=NOPASSWD: ALL/d" /etc/sudoers
 
-        read -p "Wanna remove ${user_name} directory? [y/n]: " yn
-        if [[ "$yn" == "y" ]]; then
+        read -p "Wanna remove ${user_name} directory? [Y/N]: " yn
+        if [[ "$yn" == "Y" || "$yn" == "y" ]]; then
             sudo rm -r /home/$user_name
         fi
         ;;
