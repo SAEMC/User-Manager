2133778b111b126acab600ed405cb357d6f2f439
SAEMC <saemc27@gmail.com>
2022-04-21 00:22:13 +0900

    Update user_manager.sh

--- user_manager.sh
+++ user_manager.sh
@@ -13,12 +13,6 @@ case $cmd in
             sudo usermod -aG sudo $user_name
             echo "${user_name} ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers >/dev/null
         fi
-
-        read -p "Wanna Install SAEMC Vim Settings? [Y/N]: " yn
-        if [[ "$yn" == "Y" || "$yn" == "y" ]]; then
-            sudo -u "$user_name" \
-            /bin/bash -c 'git clone https://github.com/SAEMC/Vim-Settings.git ${HOME}/Vim-Settings && ${HOME}/Vim-Settings/vim_settings.sh'
-        fi
         ;;
     [Dd][Ee][Ll] )
         sudo deluser $user_name
